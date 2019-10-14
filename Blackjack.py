8f4cb6e8af9f94fb6225691ff3b21db1188f2806
robertoschno <robertoschno@gmail.com>
2019-10-14 18:13:01 -0300

    'teste'

--- Blackjack.py
+++ Blackjack.py
@@ -7,6 +7,13 @@ Created on Wed Oct  2 14:43:54 2019
 """
 import random
 
+
+score = open('carteira.txt','r')
+score_jogador=[float(score.readline())]
+#print(score.readline())
+score.close()
+aposta=int(input('Quanto vc quer apostar essa rodada? '))
+
 cartas_computador = []
 cartas_jogador = []
 # código das cartas do computador
@@ -21,29 +28,53 @@ while len(cartas_jogador) != 2:
         print('Suas cartas são:{0} '.format(cartas_jogador))
 # soma das cartas do computador
 if sum(cartas_computador) == 21:
+    score_jogador.append(-aposta)
     print('Computador venceu!')
 elif sum(cartas_computador) > 21:
+    vitoria=aposta*1.5
+    score_jogador.append(vitoria)
     print('Computador perdeu!')
 # soma das cartas do jogador
 while sum(cartas_jogador) < 21:
-    escolha = str(input('Voce deseja tirar mais uma carta? s/n'))
+    escolha = str(input('Voce deseja tirar mais duas cartas? s/n'))
     if escolha == 's':
         cartas_jogador.append(random.randint(2, 11))
         cartas_jogador.append(random.randint(2, 11))
+        print('Suas novas duas cartas são: {0} '.format(cartas_jogador[-2:]))
         print('Voce tem um total de: {0}'.format(sum(cartas_jogador)))
     if escolha == 'n':
         print('O computador tem um total de: {0}'.format(sum(cartas_computador)))
         print('Voce tem um total de: {0}'.format(sum(cartas_jogador)))
         if sum(cartas_computador) > sum(cartas_jogador):
+            
+            score_jogador.append(-aposta)
+            print(score_jogador)
             print('Computador venceu!')
+            break
         else:
+            vitoria=aposta*1.5
+            score_jogador.append(vitoria)       
             print('Voce venceu')
             break
         
+
+            
+        
 if sum(cartas_jogador) > 21:
+    score_jogador.append(-aposta)
     print('Voce perdeu!')
 elif sum(cartas_jogador) == 21:
+    vitoria=aposta*1.5
+    score_jogador.append(vitoria)
     print('Blackjack, voce venceu!')
-         
+
+soma=0
+for i in score_jogador:
+    soma+=i
+print('Vc possui R$ {0:.2f}'.format(soma))
+
+s= open('carteira.txt','w')
+s.writelines(str(soma))
+s.close()
 
 
